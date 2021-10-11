2598a4f273bca112c2b29c9a1549a72b24866f19
CH_Hwang <hch950627@naver.com>
2021-10-11 22:20:05 +0900

    [feat] 회원가입 기능 추가

--- routers/joi.js
+++ routers/joi.js
@@ -7,7 +7,7 @@ module.exports = {
       .required()
       .min(4)
       .max(12)
-      .pattern(/^[a-zA-Z0-9]{4,12}$/),
+      .pattern(/^[a-zA-Z0-9!@#$%^&*]{4,12}$/),
     nickname: Joi.string()
       .required()
       .min(1)
@@ -17,7 +17,7 @@ module.exports = {
       .required()
       .min(6)
       .max(18)
-      .pattern(/^[a-zA-Z0-9]{6,18}$/),
+      .pattern(/^[a-zA-Z0-9!@#$%^&*]{6,18}$/),
     passwordCheck: Joi.ref('password'),
   }),
 };
--- routers/user.js
+++ routers/user.js
@@ -1,26 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const { users } = require('../models');
+const bcrypt = requrie('bcrypt');
+const {
+  idValidation,
+  passwordValidation,
+} = require('./controller/signupValidation');
+const saltRounds = 10;
 
 router.post('/signup', async (req, res, next) => {
   const { nickname, userId, password, passwordCheck } = req.body;
-  const userExist = await users.findOne({
-    where: {
-      userId,
-      password,
-    },
-  });
-  try {
-    if (!userExist) {
-      await users.create({ nickname, userId, password });
-      res.status(200).json({});
-    } else if (userExist) {
-      res.status(400).json({
-        errorMessage: '중복아이디입니다.',
-      });
+  if (
+    idValidation(userId) &&
+    passwordValidation(userId, password, passwordCheck)
+  ) {
+    const encryptedPassword = bcrypt.hash(password, saltRounds);
+    const userExist = await users.findOne({
+      where: {
+        userId,
+        password: encryptedPassword,
+      },
+    });
+    try {
+      if (!userExist) {
+        await users.create({ nickname, userId, password: encryptedPassword });
+        res.status(200).json({});
+      } else if (userExist) {
+        res.status(400).json({
+          errorMessage: '중복아이디입니다.',
+        });
+      }
+    } catch (err) {
+      console.error(err);
     }
-  } catch (err) {
-    console.error(err);
+  } else {
+    res.status(400).json({
+      errorMessage: 'validation Error',
+    });
   }
 });
 

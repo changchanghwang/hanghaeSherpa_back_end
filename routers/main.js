3d2b6b0e12a602ce5f15b7d3b4ccd6271fe4b364
CH_Hwang <hch950627@naver.com>
2021-10-12 16:15:16 +0900

    [style]불필요한 코드 삭제

--- routers/main.js
+++ routers/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { Todo } = require('../models');
 
-router.use('/view/:date', async (req, res, next) => {
+router.get('/view/:date', async (req, res, next) => {
   const { date } = req.params;
   const todo = await Todo.findOne({
     where: {

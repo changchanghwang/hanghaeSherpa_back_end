0b38c234e7418034a149717d5435242c3a36b4b2
Jaehyoen park <parkjh9370@naver.com>
2021-10-13 14:05:29 +0900

    post add

--- routers/main.js
+++ routers/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Todo } = require('../models');
+const loginAuth = require('../middleWares/loginAuth');
 
 router.get('/view/:date', async (req, res, next) => {
   const { date } = req.params;
@@ -10,6 +11,24 @@ router.get('/view/:date', async (req, res, next) => {
     },
   });
   res.status(200).json({ todo });
+});
+
+router.post('/post', loginAuth, async (req, res) => {
+  const user = res.locals.user;
+  const { date } = req.params;
+  const { perfection, creativity, difficulty, concentration, satisfacation } =
+    req.body;
+  await Posts.create({
+    user,
+    date,
+    perfection,
+    creativity,
+    difficulty,
+    concentration,
+    satisfacation,
+  });
+  res.status(200).json({});
+  return;
 });
 
 module.exports = router;

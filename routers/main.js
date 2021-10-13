1bd56849776ac70c2dcfb27ba5a2db788e836102
Jaehyoen park <parkjh9370@naver.com>
2021-10-13 16:50:59 +0900

    [fix]post 수정

--- routers/main.js
+++ routers/main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { Todo } = require('../models');
 const loginAuth = require('../middleWares/loginAuth');
+const moment = require('moment');
 
 router.get('/view/:date', loginAuth, async (req, res, next) => {
   const { date } = req.params; //2021-10-13
@@ -79,20 +80,35 @@ router.get('/view/:date', loginAuth, async (req, res, next) => {
 
 router.post('/post', loginAuth, async (req, res) => {
   const user = res.locals.user;
-  const { date } = req.params;
+  const date = moment().format('YYYY-MM-DD');
   const { perfection, creativity, difficulty, concentration, satisfacation } =
     req.body;
-  await Posts.create({
-    user,
-    date,
-    perfection,
-    creativity,
-    difficulty,
-    concentration,
-    satisfacation,
-  });
-  res.status(200).json({});
-  return;
+  const existUser = await Todo.findOne({ where: { user, date } });
+  if (!existUser) {
+    await Todo.create({
+      user,
+      date,
+      perfection,
+      creativity,
+      difficulty,
+      concentration,
+      satisfacation,
+    });
+    res.status(200).json({});
+    return;
+  } else {
+    await Todo.update({
+      user,
+      date,
+      perfection,
+      creativity,
+      difficulty,
+      concentration,
+      satisfacation,
+    });
+    res.status(200).json({});
+    return;
+  }
 });
 
 module.exports = router;

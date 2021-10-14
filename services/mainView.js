64eeafc4551c649f1d69d0f734c6bb775dfc1f45
CH_Hwang <hch950627@naver.com>
2021-10-15 00:14:06 +0900

    [feat] 메인 뷰 경우의수 추가

--- services/mainView.js
+++ services/mainView.js
@@ -1,10 +1,9 @@
 const Todo = require('../models/todos');
 
 exports.mainView = async (req, res, next) => {
-  const date = req.params;
-  console.log(date);
+  const { date } = req.params;
   const days = date.split('-');
-  const day = Number(days['2']) - 1;
+  const day = String(Number(days['2']) - 1);
   const yesterday = `${days[0]}-${days[1]}-${day}`;
   const user = res.locals.user;
   const todos = await Todo.findOne({
@@ -19,58 +18,174 @@ exports.mainView = async (req, res, next) => {
       user,
     },
   });
-  const todo = {
-    id: todos.user,
-    date: todos.date,
-    data: [
-      {
-        x: '완성도',
-        y: todos.perfection,
-      },
-      {
-        x: '창의성',
-        y: todos.creativity,
-      },
-      {
-        x: '난이도',
-        y: todos.difficulty,
-      },
-      {
-        x: '집중도',
-        y: todos.concentration,
-      },
-      {
-        x: '만족도',
-        y: todos.satisfaction,
-      },
-    ],
-  };
-  const yesterdayTodo = {
-    id: yesterdayTodos.user,
-    date: yesterdayTodos.date,
-    data: [
-      {
-        x: '완성도',
-        y: yesterdayTodos.perfection,
-      },
-      {
-        x: '창의성',
-        y: yesterdayTodos.creativity,
-      },
-      {
-        x: '난이도',
-        y: yesterdayTodos.difficulty,
-      },
-      {
-        x: '집중도',
-        y: yesterdayTodos.concentration,
-      },
-      {
-        x: '만족도',
-        y: yesterdayTodos.satisfaction,
-      },
-    ],
-  };
-  console.log(todo, yesterdayTodo);
-  res.status(200).json({ todo, yesterdayTodo });
+  if (todos && yesterdayTodos) {
+    const todo = {
+      id: todos.user,
+      date: todos.date,
+      data: [
+        {
+          x: '완성도',
+          y: todos.perfection,
+        },
+        {
+          x: '창의성',
+          y: todos.creativity,
+        },
+        {
+          x: '난이도',
+          y: todos.difficulty,
+        },
+        {
+          x: '집중도',
+          y: todos.concentration,
+        },
+        {
+          x: '만족도',
+          y: todos.satisfaction,
+        },
+      ],
+    };
+    const yesterdayTodo = {
+      id: yesterdayTodos.user,
+      date: yesterdayTodos.date,
+      data: [
+        {
+          x: '완성도',
+          y: yesterdayTodos.perfection,
+        },
+        {
+          x: '창의성',
+          y: yesterdayTodos.creativity,
+        },
+        {
+          x: '난이도',
+          y: yesterdayTodos.difficulty,
+        },
+        {
+          x: '집중도',
+          y: yesterdayTodos.concentration,
+        },
+        {
+          x: '만족도',
+          y: yesterdayTodos.satisfaction,
+        },
+      ],
+    };
+    res.status(200).json({ todo, yesterdayTodo });
+  } else if (!yesterdayTodos) {
+    const todo = {
+      id: todos.user,
+      date: todos.date,
+      data: [
+        {
+          x: '완성도',
+          y: todos.perfection,
+        },
+        {
+          x: '창의성',
+          y: todos.creativity,
+        },
+        {
+          x: '난이도',
+          y: todos.difficulty,
+        },
+        {
+          x: '집중도',
+          y: todos.concentration,
+        },
+        {
+          x: '만족도',
+          y: todos.satisfaction,
+        },
+      ],
+    };
+    const [Year, Month, days] = todos.date.split['-'];
+    const yesterdate = String(Number(days) - 1);
+    const dummydate = `${Year}-${Month}-${yesterdate}`;
+    const yesterdayTodo = {
+      id: todos.user,
+      date: dummydate,
+      data: [
+        {
+          x: '완성도',
+          y: 0,
+        },
+        {
+          x: '창의성',
+          y: 0,
+        },
+        {
+          x: '난이도',
+          y: 0,
+        },
+        {
+          x: '집중도',
+          y: 0,
+        },
+        {
+          x: '만족도',
+          y: 0,
+        },
+      ],
+    };
+    res.status(200).json({ todo, yesterdayTodo });
+  } else if (!todos) {
+    const todo = {
+      id: yesterdayTodos.user,
+      date: date,
+      data: [
+        {
+          x: '완성도',
+          y: 0,
+        },
+        {
+          x: '창의성',
+          y: 0,
+        },
+        {
+          x: '난이도',
+          y: 0,
+        },
+        {
+          x: '집중도',
+          y: 0,
+        },
+        {
+          x: '만족도',
+          y: 0,
+        },
+      ],
+    };
+    const yesterdayTodo = {
+      id: yesterdayTodos.user,
+      date: yesterdayTodos.date,
+      data: [
+        {
+          x: '완성도',
+          y: yesterdayTodos.perfection,
+        },
+        {
+          x: '창의성',
+          y: yesterdayTodos.creativity,
+        },
+        {
+          x: '난이도',
+          y: yesterdayTodos.difficulty,
+        },
+        {
+          x: '집중도',
+          y: yesterdayTodos.concentration,
+        },
+        {
+          x: '만족도',
+          y: yesterdayTodos.satisfaction,
+        },
+      ],
+    };
+    res.status(200).json({ todo, yesterdayTodo });
+  } else {
+    res.status(400).json({
+      msg: '데이터가 없습니다.',
+    });
+  }
 };
